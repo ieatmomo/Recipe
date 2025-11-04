c5be111f396ffaba300350513acea8323a146890
ieatmomo <deeveshrai125@gmail.com>
2025-11-04 20:39:06 +0000

    Fixed Structure

---
+++ recipe-micoservices/kafka_service/src/main/java/com/recipe/kafka_service/KafkaApplication.java
@@ -0,0 +1,11 @@
+package com.recipe.kafka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class kafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+}
